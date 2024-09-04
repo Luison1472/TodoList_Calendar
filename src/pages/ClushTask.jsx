27e15b83ddceaae7fb803eb8266e8f436650944d
Luison1472 <melody9908@naver.com>
2024-09-04 16:34:42 +0900

    Header 레이아웃 진행중

--- src/pages/ClushTask.jsx
+++ src/pages/ClushTask.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import Calendar from './calendar';
+import Header from '../components/Layouts/Header';
+import Footer from '../components/Layouts/Footer';
 import '../index.css';
 
 function ClushTask() {
   
 
   return (
-    <></>
+      <div>
+          <Header />
+          <Calendar />
+          <Footer/>
+    </div>
   );
 }
 
