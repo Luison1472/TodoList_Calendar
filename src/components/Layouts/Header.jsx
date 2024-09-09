9202bac4213d986f40676fe14d1e8907325d207a
Luison1472 <melody9908@naver.com>
2024-09-09 13:08:57 +0900

    png 파일 public으로 이동

--- src/components/Layouts/Header.jsx
+++ src/components/Layouts/Header.jsx
@@ -5,7 +5,7 @@ function Header({currentYear, onAddClick}) {
     <header className="flex flex-col justify-between items-center p-4 bg-gray-950 border-b border-gray-700 mb-2">
       <div className="flex justify-between items-center w-full">
         <div className="flex items-center gap-5">
-          <img src="/src/assets/right-arrow-white.png" className="transform scale-x-[-1] w-5" />
+          <img src="/public/right-arrow-white.png" className="transform scale-x-[-1] w-5" />
           <h1 className="text-sm font-bold text-left text-red-500">{currentYear}년</h1>
         </div>
 
